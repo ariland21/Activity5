104e1c6d2d7b7e77ad5458f2d153134ac1cb2870
Ariland <khumairiari21@gmail.com>
2022-03-30 23:16:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity5/ClassNama.java
@@ -0,0 +1,16 @@
+package com.example.activity5;
+
+public class ClassNama {
+    private String Nama;
+
+    //inisialisasi objek
+    public ClassNama(String nama)
+    {
+        this.Nama = nama;
+    }
+    //membuat method getName untuk mengembalikan nilai ke variabel Nama
+    public String getName()
+    {
+        return this.Nama;
+    }
+}
---
+++ app/src/main/java/com/example/activity5/HomeActivity.java
@@ -0,0 +1,86 @@
+package com.example.activity5;
+
+import android.content.Intent;
+import android.support.v4.app..AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class Home_Activity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+    private ListView list;
+
+    private com.example.activity5.ListViewAdapter adapter;
+
+    String[] listNama;
+
+    public static ArrayList<com.example.activity5.ClassNama> classNamaArrayList = new ArrayList<com.example.sampleconstraintlayout.ClassNama>();
+
+    Bundle bundle = new Bundle();
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        listNama = new String[]{"Inayah", "Ilham", "Eris", "Fikri", "Maul", "Intan",
+                "Vina", "Gita", "vian ", "Lutfi"};
+
+        list = findViewById(R.id.listkontak);
+
+        classNamaArrayList = new ArrayList<>();
+
+        for (int i = 0; i < listNama.length; i++) {
+            com.example.activity5.ClassNama classNama = new com.example.activity5.ClassNama(listNama[i]);
+
+            classNamaArrayList.add(classNama);
+        }
+        adapter = new com.example.activity5.ListViewAdapter(this);
+
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String nama = classNamaArrayList.get(position).getName();
+
+                bundle.putString("a", nama.trim());
+
+                PopupMenu pm = new PopupMenu(getApplicationContext(), view);
+
+                pm.setOnMenuItemClickListener(Home_Activity.this);
+
+                pm.inflate(R.menu.popup_menu);
+
+                pm.show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.mnView:
+                intent = new Intent(getApplicationContext(), ActivityLihatData.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                break;
+            case R.id.mnedit:
+                Toast.makeText(getApplicationContext(), "Ini untuk edit kontak",
+                        Toast.LENGTH_LONG).show();
+                break;
+        }
+        return false;
+    }
+}
